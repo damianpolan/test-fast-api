f4ce9a07c948ceb4ea5f093c153023e3ad17cd02
Damian Polan <damian.polan@gmail.com>
2025-01-27 14:28:26 -0700

    pimage rem

--- app/src/processing_stages/uploader.py
+++ app/src/processing_stages/uploader.py
@@ -1,6 +1,6 @@
 print("Uploader.py Loading")
 from ..models.models import Project
-from src.models.models import Project, ProjectImage
+from src.models.models import Project
 
 
 def processAndUpload():
